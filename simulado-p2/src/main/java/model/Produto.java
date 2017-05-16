2dac191c192ded421bc5550b56d51bbb7341cebe
MatheusVilvert <matheusvilvert@gmail.com>
2017-05-15 22:04:50 -0400

    lel

---
+++ simulado-p2/src/main/java/model/Produto.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Produto {
+	
+	private int id;
+	private String nome;
+	private double valor;
+	
+	public Produto(){
+		
+	};
+	
+	public Produto(String nome,double valor){
+		this.nome = nome;
+		this.valor = valor;
+	};
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+
+}
